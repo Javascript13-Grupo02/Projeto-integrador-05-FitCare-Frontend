5716cdba9458c0c554dacc5b5eb11d7cf3ebc2aa
paula-alcantara <paullety.m@gmail.com>
2026-04-16 11:38:37 -0300

    ✨ New Feature: Service

---
+++ src/services/Service.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+})
+
+// Consultar
+export const buscar = async(url: string, setDados: Function) => {
+  const resposta = await api.get(url);
+  setDados(resposta.data)
+}
+
+// Cadastrar
+export const cadastrar = async(url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data)
+}
+
+// Atualizar
+export const atualizar = async(url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.put(url, dados);
+  setDados(resposta.data)
+}
+
+// Deletar
+export const deletar = async(url: string) => {
+  await api.delete(url);
+}
