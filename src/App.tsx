e5c17c94780927dff68f6c60a5796583caa4faaf
wallaceespy <wallace17.esp@gmail.com>
2026-04-16 11:55:51 -0300

    ✨ New Feature: Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
 
 
 function App() {
@@ -8,7 +9,11 @@ function App() {
     <>
           <BrowserRouter>
                   <Navbar />
+                          <div className="min-h-[80vh]">
 
+        </div>
+
+                    <Footer />
 
             </BrowserRouter>
 
