6594d490233d4d239f56d3aaa6344daf2d1da21c
wallaceespy <wallace17.esp@gmail.com>
2026-04-16 11:32:39 -0300

    ✨ New Feature: Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
 
 
 function App() {
 
   return (
     <>
-      
+          <BrowserRouter>
+                  <Navbar />
+
+
+            </BrowserRouter>
+
     </>
   )
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <div className='w-full flex justify-center py-4 bg-teal-900 text-white'>
+      <div className="container flex justify-between text-lg mx-8">
+
+        <Link to='/home' className="flex items-center gap-2 text-2xl font-bold">
+          <img 
+            src="https://ik.imagekit.io/vjqejp2vh/FitCare%20Logo%20Clara.png" 
+            alt="FitCare Logo" 
+            className="h-9 w-auto"
+          />
+          FitCare
+        </Link>
+
+        <div className='flex gap-4'>
+          <Link to='/categoria' className='hover:text-emerald-500 transition-colors'>Categoria</Link>
+          <Link to='/exercicio' className='hover:text-emerald-500 transition-colors'>Exercício</Link>
+          <Link to='/sobre' className='hover:text-emerald-500 transition-colors'>Sobre</Link>
+          <Link to='/equipe' className='hover:text-emerald-500 transition-colors'>Equipe</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
