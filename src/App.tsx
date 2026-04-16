6eb769126665406384744cdc95178edb6a6726f7
wallaceespy <wallace17.esp@gmail.com>
2026-04-16 14:25:54 -0300

    ✨ New Feature:Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
+import Home from "./pages/home/Home"
 
 
 function App() {
 
   return (
     <>
-          <BrowserRouter>
-                  <Navbar />
-                          <div className="min-h-[80vh]">
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+                      <Route path="/" element={<Home />}/>
+                      <Route path="/home" element={<Home />}/>
+
+          </Routes>
 
         </div>
 
-                    <Footer />
+        <Footer />
 
-            </BrowserRouter>
+      </BrowserRouter>
 
     </>
   )
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,19 @@
+
+
+function Home() {
+  return (
+    <>
+      <section className="bg-orange-100 flex flex-col items-center justify-center pt-8 bg-[url('https://')] bg-cover bg-center relative h-dvh">
+        <div className="absolute inset-0 bg-black/50">
+        </div>
+        <div className="z-10 flex flex-col items-center justify-center gap-4">
+          <h1 className="text-3xl md:text-5xl font-bold  text-white text-center m-2"></h1>
+          <p className="text-2xl md:text-3xl font-medium leading-snug text-white text-center m-2"></p>
+        </div>
+      </section>
+      
+    </>
+  )
+}
+
+export default Home
