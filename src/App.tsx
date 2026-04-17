aff7adebc856fc526746dc315d1a1d6f092a6bdc
paula-alcantara <paullety.m@gmail.com>
2026-04-17 12:58:11 -0300

    🔀 Merging: Calculadora 3

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/sobre" element={<Sobre />} />
             <Route path="/equipe" element={<Equipe />} />
-            {/* <Route path="/calculadora" element={<Calcula />} /> */}
             <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
             <Route path="/cadastrarcategoria" element={<FormCategoria />} />
             <Route path="/editarcategoria/:id" element={<FormCategoria />} />
