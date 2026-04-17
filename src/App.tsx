699db6c6e1c013a532bc7ce5ae143502a69a2070
Vivian Rodrigues <zueleigafar@gmail.com>
2026-04-17 12:40:11 -0300

    ✨ New Feature: Cadastro de Exercício

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListaExercicios from "./components/exercicio/listaexercicio/ListaExercicio";
 import ListaCategorias from "./components/categoria/listacategoria/ListaCategoria";
-
 import FormContato from "./components/form/Form";
 import DeletarExercicio from "./components/exercicio/deletarexercicio/DeletarExercicio";
+import FormExercicio from "./components/exercicio/formexercicio/FormExercicio";
 
 function App() {
   return (
@@ -14,6 +14,8 @@ function App() {
         <Route path="/contato" element={<FormContato />} />
         <Route path="/" element={<ListaExercicios />} />
                 <Route path="/deletarexercicio/:id" element={<DeletarExercicio />} />
+        <Route path="/cadastrarexercicio" element={<FormExercicio />} />
+        <Route path="/editarexercicio/:id" element={<FormExercicio />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/exercicio/deletarexercicio/DeletarExercicio.tsx
+++ src/components/exercicio/deletarexercicio/DeletarExercicio.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
 import type Exercicio from "../../../models/Exercicio";
 import { buscar, deletar } from "../../../services/Service";
+import { CheckIcon, XIcon } from "@phosphor-icons/react";
 
 function DeletarExercicio() {
   const navigate = useNavigate();
@@ -34,6 +35,8 @@ function DeletarExercicio() {
   }
 
   return (
+
+    <div className="bg-linear-to-br from-black to-[#087f5b] text-white">
     <div className="container w-1/3 mx-auto">
       <h1 className="text-4xl text-center my-4">Deletar Exercício</h1>
 
@@ -41,7 +44,7 @@ function DeletarExercicio() {
         Você tem certeza de que deseja apagar o exercício a seguir?
       </p>
 
-      <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
+      <div className="border border-black/20 flex flex-col overflow-hidden justify-between">
         <header className="py-2 px-6  bg-teal-900 text-white font-bold text-2xl">
           Exercício
         </header>
@@ -49,25 +52,30 @@ function DeletarExercicio() {
           <p className="text-xl h-full">{exercicio.nome}</p>
           <p>{exercicio.tipo}</p>
         </div>
+        
         <div className="flex">
-          <button
-            className="text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2"
-            onClick={retornar}
-          >
-            Não
-          </button>
-          <button
-            className="w-full text-slate-100 bg-indigo-400 hover:bg-teal-900 flex items-center justify-center"
-            onClick={deletarExercicio}
-          >
-            {isLoading ? (
-              <ClipLoader color="#ffffff" size={24} />
-            ) : (
-              <span>Sim</span>
-            )}
-          </button>
+
+        <div className="grid grid-cols-2 border-t border-white/6 w-full">
+          <a
+            className="flex items-center justify-center gap-2 py-3 text-xs text-white/40 font-semibold uppercase tracking-widest border-white bg-red-800/50 hover:bg-red-700/70  hover:text-white transition-colors duration-200"
+            onClick={retornar}>
+            <XIcon size={32} color="#ffffff" /> Não
+          </a>
+          
+          <a
+            className="flex items-center justify-center gap-2 py-3 text-white/40 text-xs font-semibold uppercase tracking-widest hover:bg-emerald-400/70 hover:text-white transition-colors duration-200"
+            onClick={deletarExercicio}>
+            <CheckIcon size={32} color="#ffffff"/>
+              {isLoading ? (
+                <ClipLoader color="#ffffff" size={24} />
+              ) : (
+                <span>Sim</span>
+              )}
+          </a>
+        </div>        
         </div>
       </div>
+    </div>  
     </div>
   );
 }
